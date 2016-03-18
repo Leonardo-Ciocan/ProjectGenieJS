94adba5baf2c158c769aee0d5e4c3564bb008220
Leonardo C <leonardo.ciocan@outlook.com>
2016-03-18 18:46:24 +0000

    Added syntax highlighting

--- ProjectGenieJS/js/out/IndexPage.js
+++ ProjectGenieJS/js/out/IndexPage.js
@@ -65,7 +65,10 @@ var MessageControl = (function (_super) {
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat"
         };
-        return React.createElement("div", {onClick: function () { return _this.props.onClick(_this.props.message.id); }, style: messageStyle}, React.createElement("div", {style: circleStyle}), React.createElement("h1", {style: nameStyle, onClick: function (event) { _this.props.onClickUser(_this.props.message.authorID); event.stopPropagation(); }}, (this.props.service != undefined ? this.props.service.name : State.name)), React.createElement("h1", {style: atStyle}, "@" + (this.props.service != undefined ? this.props.service.name.toLowerCase() : State.username)), React.createElement("div", {style: bodyStyle}, this.props.message.body));
+        var reg = /\B@[a-z0-9_-]+/gi;
+        var final_str = this.props.message.body.replace(reg, function (str) { return '<span style="color:' + (_this.props.service == undefined ? "black" : _this.props.service.color) + '">' + str + '</span>'; });
+        console.log(final_str);
+        return React.createElement("div", {onClick: function () { return _this.props.onClick(_this.props.message.id); }, style: messageStyle}, React.createElement("div", {style: circleStyle, onClick: function (event) { _this.props.onClickUser(_this.props.message.authorID); event.stopPropagation(); }}), React.createElement("h1", {style: nameStyle, onClick: function (event) { _this.props.onClickUser(_this.props.message.authorID); event.stopPropagation(); }}, (this.props.service != undefined ? this.props.service.name : State.name)), React.createElement("h1", {style: atStyle, onClick: function (event) { _this.props.onClickUser(_this.props.message.authorID); event.stopPropagation(); }}, "@" + (this.props.service != undefined ? this.props.service.name.toLowerCase() : State.username)), React.createElement("div", {style: bodyStyle, dangerouslySetInnerHTML: { __html: final_str }}));
     };
     return MessageControl;
 }(React.Component));
@@ -111,7 +114,10 @@ var ThreadPage = (function (_super) {
             width: "100%", height: "100%",
             overflow: "hidden"
         };
+        var services = [];
         var messages = getMessagesByThread(this.props.thread.id).map(function (message) {
+            if (services.indexOf(message.authorID) == -1 && message.authorID != "")
+                services.push(message.authorID);
             return React.createElement(MessageControl, {onClickUser: function (id) { return _this.props.goto(Page.Profile, { id: id }); }, key: message.id, message: message, service: getService(message.authorID)});
         });
         return React.createElement("div", {style: containerStyle}, React.createElement("div", {style: { position: "absolute", left: "0", right: "0", bottom: "45px", top: "40px", overflowY: "scroll" }}, messages), React.createElement("div", {style: inputParentStyle}, React.createElement("a", {href: "#", style: {
@@ -121,7 +127,7 @@ var ThreadPage = (function (_super) {
             lineHeight: "45px",
             verticalAlign: "middle",
             right: "10px", top: "0px", bottom: "0px"
-        }}, "Send"), React.createElement("input", {style: inputStyle, placeholder: "Enter message", type: "text", onKeyPress: this.textKeyDown, className: "win-textbox"})));
+        }}, "Send"), React.createElement("input", {defaultValue: "@" + State.services.filter(function (s) { return s.id == services[0]; })[0].name, style: inputStyle, placeholder: "Enter message", type: "text", onKeyPress: this.textKeyDown, className: "win-textbox"})));
     };
     return ThreadPage;
 }(React.Component));
@@ -198,7 +204,16 @@ var ProfilePage = (function (_super) {
             borderRadius: "5px",
             cursor: "pointer"
         };
-        return React.createElement("div", {style: containerStyle}, React.createElement("div", {style: circleStyle}), React.createElement("div", {style: header}, " "), React.createElement("h1", {style: nameStyle}, (this.props.service != undefined ? this.props.service.name : State.name)), React.createElement("h1", {style: atStyle}, "@" + (this.props.service != undefined ? this.props.service.name.toLowerCase() : State.username)), React.createElement("div", {style: followButton}, "Follow"), messages);
+        var descriptionStyle = {
+            fontFamily: "Segoe UI , Helvetica ,  serif",
+            display: "block", fontSize: "15pt",
+            color: "black",
+            margin: "10px",
+            fontWeight: "200",
+            verticalAlign: "middle", textAlign: "center",
+            marginBottom: "15px"
+        };
+        return React.createElement("div", {style: containerStyle}, React.createElement("div", {style: circleStyle}), React.createElement("div", {style: header}, " "), React.createElement("h1", {style: nameStyle}, (this.props.service != undefined ? this.props.service.name : State.name)), React.createElement("h1", {style: atStyle}, "@" + (this.props.service != undefined ? this.props.service.name.toLowerCase() : State.username)), React.createElement("div", null, React.createElement("h1", {style: descriptionStyle}, this.props.service.description)), React.createElement("div", {style: followButton}, "Follow"), messages);
     };
     return ProfilePage;
 }(React.Component));
@@ -206,7 +221,43 @@ var Page;
 (function (Page) {
     Page[Page["Profile"] = 0] = "Profile";
     Page[Page["Thread"] = 1] = "Thread";
+    Page[Page["Feed"] = 2] = "Feed";
 })(Page || (Page = {}));
+var FeedPage = (function (_super) {
+    __extends(FeedPage, _super);
+    function FeedPage(props) {
+        var _this = this;
+        _super.call(this, props);
+        this.threadClicked = function (id) {
+            _this.props.goto(Page.Thread, { id: id });
+        };
+        this.gotoProfile = function (id) {
+            _this.props.goto(Page.Profile, { id: id });
+        };
+        this.state = {};
+    }
+    FeedPage.prototype.render = function () {
+        var _this = this;
+        var containerStyle = {
+            width: "100%", height: "100%", background: "white",
+            overflowY: "scroll",
+            position: "relative",
+            paddingTop: "55px"
+        };
+        var messages = getFeed().map(function (message) {
+            return React.createElement(MessageControl, {onClickUser: _this.gotoProfile, onClick: function () { return _this.threadClicked(message.threadID); }, key: message.id, message: message, service: getService(message.authorID)});
+        });
+        var bottomBar = {
+            height: "50px",
+            borderBottom: "1px solid lightgray",
+            background: "rgba(0,0,0,0.1)",
+            position: "absolute",
+            left: "0px", top: "0px", right: "0px"
+        };
+        return React.createElement("div", {style: containerStyle}, messages, React.createElement("div", {style: bottomBar}));
+    };
+    return FeedPage;
+}(React.Component));
 var App = (function (_super) {
     __extends(App, _super);
     function App(props) {
@@ -226,7 +277,7 @@ var App = (function (_super) {
             _this.setState({});
         };
         this.state = {
-            pages: [{ page: Page.Profile, data: { id: "0" } }],
+            pages: [{ page: Page.Feed, data: { id: "0" } }],
             selectedSection: 0
         };
     }
@@ -238,6 +289,9 @@ var App = (function (_super) {
         }
         else if (this.state.pages[this.state.pages.length - 1].page == Page.Thread) {
             content = React.createElement(ThreadPage, {goto: this.goto, thread: State.threads[this.state.pages[this.state.pages.length - 1].data.id]});
+        }
+        else if (this.state.pages[this.state.pages.length - 1].page == Page.Feed) {
+            content = React.createElement(FeedPage, {goto: this.goto});
         }
         var barStyle = {
             background: "white",
@@ -265,7 +319,7 @@ var App = (function (_super) {
                 background: index == _this.state.selectedSection ? State.color : "white"
             }});
         });
-        return React.createElement("div", null, React.createElement("div", {style: { background: "white", zIndex: this.state.selectedSection == 0 ? "99" : "0", marginLeft: "50px", position: "absolute", left: "0", top: "0", bottom: "0", right: "0" }}, content), React.createElement("div", {style: { zIndex: this.state.selectedSection == 1 ? "99" : "0", background: "Red", marginLeft: "50px", position: "absolute", left: "0", top: "0", bottom: "0", right: "0" }}), React.createElement("div", {style: barStyle}, React.createElement("i", {onClick: this.back, className: "fa fa-arrow-left", style: {
+        return React.createElement("div", null, React.createElement("div", {style: { borderLeft: "1px solid lightgray", background: "white", zIndex: this.state.selectedSection == 0 ? "99" : "0", marginLeft: "50px", position: "absolute", left: "0", top: "0", bottom: "0", right: "0" }}, content), React.createElement("div", {style: { zIndex: this.state.selectedSection == 1 ? "99" : "0", background: "Red", marginLeft: "50px", position: "absolute", left: "0", top: "0", bottom: "0", right: "0" }}), React.createElement("div", {style: barStyle}, React.createElement("i", {onClick: this.back, className: "fa fa-arrow-left", style: {
             width: "50px", height: "50px", lineHeight: "50px", textAlign: "center", verticalAlign: "middle",
             background: "rgba(0,0,0,0.05)", fontSize: "20pt", color: State.color,
             visibility: this.state.pages.length == 1 ? "collapse" : "visible"
--- ProjectGenieJS/js/out/Models.js
+++ ProjectGenieJS/js/out/Models.js
@@ -20,17 +20,21 @@ var State = {
     currentThread: "1",
     threads: [
         { id: "0" },
-        { id: "1" }
+        { id: "1" },
+        { id: "2" }
     ],
     messages: [
-        { id: "0", type: 0, body: "Your weekly spending is $55", threadID: "0", authorID: "0" },
-        { id: "1", type: 0, body: "@azure What about monthly?", threadID: "0", authorID: "" },
-        { id: "2", type: 0, body: "@leonardo That would be $200", threadID: "0", authorID: "0" },
-        { id: "3", type: 0, body: "@azure Remind me to do X and Y", threadID: "1", authorID: "" },
-        { id: "4", type: 0, body: "@leonardo Cool , I'll remind you next week", threadID: "1", authorID: "0" },
+        { id: generateID(), type: 0, body: "Your weekly spending is $55", threadID: "0", authorID: "0" },
+        { id: generateID(), type: 0, body: "@azure What about monthly?", threadID: "0", authorID: "" },
+        { id: generateID(), type: 0, body: "@leonardo That would be $200", threadID: "0", authorID: "0" },
+        { id: generateID(), type: 0, body: "@azure Remind me to do X and Y", threadID: "1", authorID: "" },
+        { id: generateID(), type: 0, body: "@leonardo Cool , I'll remind you next week", threadID: "1", authorID: "0" },
+        { id: generateID(), type: 0, body: "@amazon How much is a samsung cable?", threadID: "2", authorID: "" },
+        { id: generateID(), type: 0, body: "@leonardo Cheapest is $15", threadID: "2", authorID: "1" },
     ],
     services: [
-        { id: "0", name: "Azure", color: "#00C0F5" }
+        { id: "0", description: "Azure is a cloud computing platform and infrastructure from microsoft", name: "Azure", color: "#00C0F5" },
+        { id: "1", description: "An american electronic commerce and cloud computing company", name: "Amazon", color: "orange" },
     ],
     getThread: function (id) {
         return this.threads.filter(function (thread) { return thread.id == id; })[0];
@@ -52,4 +56,10 @@ function getMessagesByThread(threadID) {
 function getMessagesByService(serviceID) {
     var messages = State.messages;
     return messages.slice().filter(function (message) { return message.authorID == serviceID; });
+}
+function getFeed() {
+    return State.threads.map(function (thread) {
+        var msgs = getMessagesByThread(thread.id);
+        return msgs[msgs.length - 1];
+    });
 }
